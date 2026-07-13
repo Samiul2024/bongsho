47bd17cf0999e9685432b04fee663d753ec7595b
MD. Samiulla Hossen <157939419+Samiul2024@users.noreply.github.com>
2026-07-13 12:27:03 +0600

    fix: import useeffect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import SEO from "../components/SEO";
 import AddPersonForm from "../components/AddPersonForm";
 
